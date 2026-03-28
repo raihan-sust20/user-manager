f0534e48c1cd1d9baa2694b2d74d0667e5899c7d
Raihan <raihan.sust20@gmail.com>
2026-03-29 00:28:22 +0600

    feat: create home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Container, Flex, Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full">
+        <Theme accentColor="tomato">
+        <Container size="4" mx="4">
+          {/* <Flex direction="column">{children}</Flex>
+           */}
+           {children}
+        </Container>
+        </Theme>
+      </body>
     </html>
   );
 }
